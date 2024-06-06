e756514dd10f7d0eebe051d6985ea2c2a9828dab
Masson, Lola <lola.masson@ifm.com>
2024-06-06 13:20:37 -0400

    Add getPallet c++ example and cleanup python example

--- PDS/Python/get_pallet.py
+++ PDS/Python/get_pallet.py
@@ -14,13 +14,15 @@ import numpy as np
 from ifm3dpy.device import O3R
 from ifm3dpy.framegrabber import FrameGrabber, buffer_id
 
-# Edit for the IP address of your OVP8xx and the camera port
+# Device specific configuration
 IP = "192.168.0.69"
 CAMERA_PORT = "port2"
 APP_PORT = "app0"
-
 o3r = O3R(IP)
 
+############################################
+# Setup the application
+############################################
 # Ensure a clean slate before running the example
 try:
     o3r.reset("/applications/instances")
@@ -28,24 +30,36 @@ except Exception as e:
     print(f"Reset failed: {e}")
 
 # Set the extrinsic calibration of the camera
-c = {"transX": 0.0, "transY": 0, "transZ": 0.0, "rotX": -1.57, "rotY": 1.57, "rotZ": 0}
+calibration = {
+    "transX": 0.0,
+    "transY": 0.0,
+    "transZ": 0.2,
+    "rotX": 0.0,
+    "rotY": 1.57,
+    "rotZ": -1.57,
+}
 print(f"Setting extrinsic calibration for {CAMERA_PORT}")
-o3r.set({"ports": {CAMERA_PORT: {"processing": {"extrinsicHeadToUser": c}}}})
+o3r.set({"ports": {CAMERA_PORT: {"processing": {"extrinsicHeadToUser": calibration}}}})
 
-# Create the PDS application and
-# choose the camera port
 print(f"Creating a PDS instance with camera in {CAMERA_PORT}")
 o3r.set(
-    {"applications": {"instances": {APP_PORT: {"class": "pds", "ports": [CAMERA_PORT]}}}}
+    {
+        "applications": {
+            "instances": {
+                APP_PORT: {"class": "pds", "ports": [CAMERA_PORT], "state": "IDLE"}
+            }
+        }
+    }
 )
-
-# Set the application to IDLE (ready to be triggered)
-print("Setting the PDS application to IDLE")
-o3r.set({"applications": {"instances": {APP_PORT: {"state": "IDLE"}}}})
 
 time.sleep(0.5)
 # %%
-fg = FrameGrabber(o3r, 51010)
+############################################
+# Setup the framegrabber to receive frames
+# when the application is triggered.
+############################################
+fg = FrameGrabber(o3r, o3r.port(APP_PORT).pcic_port)
+fg.start([buffer_id.O3R_RESULT_JSON])
 
 
 # Define a callback to be executed when a frame is received
@@ -60,16 +74,20 @@ def pallet_callback(frame):
         json_array = np.frombuffer(json_chunk[0], dtype=np.uint8)
         json_array = json_array.tobytes()
         parsed_json_array = json.loads(json_array.decode())
-        print(f"Pallet: {parsed_json_array['getPallet']['pallet']}")
-        print(f"Full getPallet array: {parsed_json_array}")
+        print(f"Detected pallet(s): {parsed_json_array['getPallet']['pallet']}")
 
-fg.start([buffer_id.O3R_RESULT_JSON])
+
 fg.on_new_frame(pallet_callback)
 
+############################################
+# Trigger the volCheck command
+############################################
+time.sleep(2)  # Grace period after the framegrabber starts
+
+# Provide the estimated distance to the pallet and the pallet type.
 GET_PALLET_PARAMETERS = {
-    "depthHint": -1,
+    "depthHint": 1.2,  # We recommend providing a depth hint for faster detections
     "palletIndex": 0,  # Block Pallet/EPAL pallet
-    "palletOrder": "scoreDescending",
 }
 
 # %%
